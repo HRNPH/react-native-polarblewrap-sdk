c8830bae66dc83fe0372d22b75f9cec454020105
HRNPH <51855316+HRNPH@users.noreply.github.com>
2023-05-23 14:12:16 +0700

    build: kotlin polar sdk

--- android/src/main/java/com/polarblewrapsdk/PolarblewrapSdkModule.kt
+++ android/src/main/java/com/polarblewrapsdk/PolarblewrapSdkModule.kt
@@ -1,12 +1,24 @@
 package com.polarblewrapsdk
+package com.polar.androidblesdk
 
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
-class PolarblewrapSdkModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+import com.polar.sdk.api.PolarBleApi
+import com.polar.sdk.api.PolarBleApiCallback
+import com.polar.sdk.api.PolarBleApiDefaultImpl
+import com.polar.sdk.api.PolarH10OfflineExerciseApi
+import com.polar.sdk.api.errors.PolarInvalidArgument
+import com.polar.sdk.api.model.*
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.*
+
+class PolarblewrapSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
   override fun getName(): String {
     return NAME
@@ -18,6 +30,9 @@ class PolarblewrapSdkModule(reactContext: ReactApplicationContext) :
   fun multiply(a: Double, b: Double, promise: Promise) {
     promise.resolve(a * b)
   }
+
+  // polar ble sdk stuff
+  private val applicationContext = reactContext.applicationContext
 
   companion object {
     const val NAME = "PolarblewrapSdk"
